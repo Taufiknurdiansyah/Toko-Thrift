26b80443fd7a766f02e58e090b2c4feb498244b4
Taufik Nurdiansyah <93576072+Taufiknurdiansyah@users.noreply.github.com>
2022-01-10 16:35:19 +0700

    Add files via upload

---
+++ storage/framework/views/7db0b4c101eb3bcca09c051d2454c07bb97cd67c.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>IMONO.ID</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo e(('web/assets/css/styles.css')); ?>" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">IMONO.ID</a>
+                <nav class="navbar">Thrift Shop </nav>
+                </div>
+            </div>
+        </nav>
+        <!-- Product section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="row gx-4 gx-lg-5 align-items-center">
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="<?php echo e(('web/assets/images/img1.jpg')); ?>" alt="..." /></div>
+                    <div class="col-md-6">
+                        <div class="small mb-1">Code: 001</div>
+                        <h1 class="display-5 fw-bolder">T-SHIRT</h1>
+                        <div class="fs-5 mb-5">
+                            <span class="text-decoration-line-through">Rp.50.000</span>
+                            <span>Rp.45.000</span>
+                        </div>
+                        <p class="lead">T-Shirt Oversize Mulus</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Related items section-->
+        <section class="py-5 bg-light">
+            <div class="container px-4 px-lg-5 mt-5">
+                <h2 class="fw-bolder mb-4">Produk Tersedia</h2>
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?php echo e(('web/assets/images/img2.jpg')); ?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">CREWNECK</h5>
+                                    <!-- Product price-->
+                                    Rp.40.000 - Rp.80.000
+                                    <a href="http://instagram.com/imono.id"><button class="btn btn-danger" type="button">Instagram</button></a>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                           
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?php echo e(('web/assets/images/img3.jpg')); ?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">VINTAGE JAKET</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">Rp.85.000</span>
+                                    Rp.70.000
+                                    <a href="http://instagram.com/imono.id"><button class="btn btn-danger" type="button">Instagram</button></a>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                           
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?php echo e(('web/assets/images/img4.jpg')); ?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">KEMEJA FLANEL</h5>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">Rp.60.000</span>
+                                    Rp.50.000
+                                    <a href="http://instagram.com/imono.id"><button class="btn btn-danger" type="button">Instagram</button></a>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?php echo e(('web/assets/images/img5.jpg')); ?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">HOODIE</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    Rp.50.000 - Rp.90.000
+                                    <a href="http://instagram.com/imono.id"><button class="btn btn-danger" type="button">Instagram</button></a>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; imono.id</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo e(('web/assets/js/scripts.js')); ?>"></script>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\web\resources\views/main.blade.php ENDPATH**/ ?>
